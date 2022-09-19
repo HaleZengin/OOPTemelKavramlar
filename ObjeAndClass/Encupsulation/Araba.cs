f220e37584b88ef288da470d9ea63e256b997d0b
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-09-20 00:04:36 +0300

    Encupsulation

---
+++ ObjeAndClass/Encupsulation/Araba.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encupsulation
+{
+    internal class Araba
+    {
+
+        int hiz = 0;
+        bool motorCalisiyorMu = false;
+        public void GazaBas()
+        {
+            if (motorCalisiyorMu)
+            {
+                hiz++;
+            }
+        }
+
+        public void Calistir()
+        {
+            motorCalisiyorMu = true;
+        }
+
+    }
+}
---
+++ ObjeAndClass/Encupsulation/Urun.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encupsulation
+{
+    internal class Urun
+    {
+
+        public string Ad;
+        private decimal fiyat;
+
+        public void SetFiyat(decimal deger)
+        {
+            if (deger > 0)
+            {
+                fiyat = deger;
+            }
+            else
+            {
+                throw new Exception("Değer 0'dan büyük olmalıdır!");
+            }
+
+        }
+
+        public decimal GetFiyat()
+        {
+            return fiyat;
+        }
+
+    }
+}
