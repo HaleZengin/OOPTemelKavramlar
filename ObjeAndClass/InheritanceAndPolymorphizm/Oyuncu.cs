a2b1d0cb94ccfd647cf9922245699e9a380437d4
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-09-20 00:16:14 +0300

    InheritanceAndPolymorphizm

---
+++ ObjeAndClass/InheritanceAndPolymorphizm/Oyuncu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAndPolymorphizm
+{
+    internal class Oyuncu
+    {
+        public string Nick { get; set; }
+
+        public Silah OyuncununSilahi { get; set; }
+
+    }
+
+    public class Silah
+    {
+        public int Agirlik { get; set; }
+        public int VurusGucu { get; set; }
+        public void Atak()
+        {
+
+        }
+    }
+
+    public class Bicak : Silah      // bicak silah'a extends yapilmis demek
+    {
+
+    }
+    public class AtesliSilah : Silah
+    {
+        public bool SeriMi { get; set; }
+        public int MaksimumMermi { get; set; }
+    }
+
+    public class Tufek : AtesliSilah
+    {
+        public bool DurbunVarMi { get; set; }
+    }
+
+    public class Ak47 : Tufek
+    {
+
+    }
+}
+
---
+++ ObjeAndClass/InheritanceAndPolymorphizm/Yemek.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAndPolymorphizm
+{
+    public class Yemek
+    {
+
+        public List<string> Malzemeler { get; set; }
+        public int PismeSuresi { get; set; }
+        public string Aciklama { get; set; }
+
+        public void Pisir()
+        {
+            Console.WriteLine($"{PismeSuresi} dakika boyunca {GetType().Name} yemeği pişirildi");
+        }
+
+        //Eğer İSTENİRSE (Gerekirse) ez: override et 
+        public virtual void SunumYap()
+        {
+            Console.WriteLine($"{GetType().Name} Yanında pilav ile sunuluyor.");
+        }
+
+
+    }
+
+    public class SebzeYemegi : Yemek
+    {
+        public bool ZeytinYagliMi { get; set; }
+
+    }
+
+    public class Pirasa : SebzeYemegi
+    {
+        public bool HavucVarMi { get; set; }
+    }
+
+    public class EtYemegi : Yemek
+    {
+        public string EtTuru { get; set; }
+    }
+
+    public class Kofte : EtYemegi
+    {
+
+    }
+
+    public class TulumbaTatlisi : Yemek
+    {
+        public override void SunumYap()
+        {
+            Console.WriteLine($"{GetType().Name} Yanında dondurma ile sunuluyor.");
+        }
+
+    }
+}
