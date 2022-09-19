118e2f96f28390d32d6e7a3c3742cfbd70bd6904
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-09-20 00:30:14 +0300

    Abstraction

---
+++ ObjeAndClass/Abstraction/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataSource excel = new ExcelDataSource();
+
+        }
+    }
+}
