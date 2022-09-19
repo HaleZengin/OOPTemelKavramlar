1e7ccf59c3eef02204329b4c5ffce697e60d027b
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-09-19 22:49:06 +0300

    Constructor

---
+++ ObjeAndClass/Constructor/CamasirMakinesi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class CamasirMakinesi
+    {
+
+        public string Renk { get; set; }
+        public int RotatePerMinute { get; set; }
+
+        public int Kapasite { get; set; }
+
+        // parametresiz constructor
+
+        //public CamasirMakinesi()
+        //{
+        //    Renk = "Beyaz";
+        //    RotatePerMinute = 1200;
+        //    Kapasite = 10;
+        //}
+
+        // parametresiz constructor olustur
+        public CamasirMakinesi(string renk)
+        {
+            Renk = renk;
+        }
+
+
+        public CamasirMakinesi(int kapasite) : this("Beyaz")
+        {
+            Kapasite = kapasite;
+            //Renk = "Beyaz";
+
+        }
+
+        public CamasirMakinesi(int kapasite, int rpm) : this("Beyaz")
+        {
+            Kapasite = kapasite;
+            RotatePerMinute = rpm;
+            //Renk = "Beyaz";
+
+        }
+
+    }
+}
---
+++ ObjeAndClass/Constructor/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Constructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CamasirMakinesi camasirMakinesi = new CamasirMakinesi(1200);
+            Console.WriteLine(camasirMakinesi.Renk);
+
+            CamasirMakinesi cm = new CamasirMakinesi(15, 2000);
+        }
+    }
+}
