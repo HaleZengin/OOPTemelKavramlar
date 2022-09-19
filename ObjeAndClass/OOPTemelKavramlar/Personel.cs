3ad76013b1d29a4f7da803ad86807e0116aec419
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-09-19 16:55:21 +0300

    ObjeAndClass

---
+++ ObjeAndClass/OOPTemelKavramlar/Personel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPTemelKavramlar
+{
+    internal class Personel
+    {
+        public string ad;
+        public int yas;
+        public string soyad;
+    }
+}
