b942f71e9ded817036c514b1e77293078124f22c
HaleZengin <98224494+HaleZengin@users.noreply.github.com>
2022-09-21 00:02:22 +0300

    Interface

---
+++ ObjeAndClass/Interface/Person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Person
+    {
+
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        public IAddress Address { get; set; }
+    }
+
+    public interface IAddress
+    {
+        string City { get; set; }
+        string Street { get; set; }
+        string Country { get; set; }
+    }
+
+    public class HomeAddress : IAddress
+    {
+        public string City { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Street { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Country { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+
+    public class WorkAddress : IAddress
+    {
+        public string City { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Street { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Country { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    }
+}
